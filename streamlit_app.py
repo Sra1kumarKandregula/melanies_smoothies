eb3d3314d8e8ccf76db0debd48128e15c2be4a51
Sra1kumarKandregula <48097018+Sra1kumarKandregula@users.noreply.github.com>
2025-05-21 22:00:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 import requests
 import pandas as pd_df
@@ -37,7 +38,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
          ingredients_string += fruit_chosen + ' '
          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
          st.subheader(fruit_chosen + ' Nutrition Information')
       
          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
